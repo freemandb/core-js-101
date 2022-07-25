436ace2a7bb01362bd0086402dc3105ebc57ea65
denis.borushko <denis.borushko@vizor-games.com>
2022-07-26 01:30:38 +0300

    93

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -182,8 +182,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startFromInt = startFrom;
+
+  return () => {
+    const res = startFromInt;
+    startFromInt += 1;
+    return res;
+  };
 }
 
 
