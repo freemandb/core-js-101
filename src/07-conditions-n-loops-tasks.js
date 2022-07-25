4b0b58ff40ab926f59fb9d750ae2ed55a43f8aaa
denis.borushko <denis.borushko@vizor-games.com>
2022-07-25 18:06:58 +0300

    86

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,17 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+  return num;
 }
 
 
@@ -43,8 +52,8 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  return (n !== 1) ? n * getFactorial(n - 1) : 1;
 }
 
 
@@ -60,8 +69,9 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  return new Array(n2 - n1 + 1).fill(0).map((el, ind) => n1 + ind)
+    .reduce((prev, cur) => prev + cur, 0);
 }
 
 
@@ -80,8 +90,8 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
 }
 
 
@@ -148,8 +158,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const d = Math.sqrt((circle.center.x - point.x) ** 2 + (circle.center.y - point.y) ** 2);
+  return circle.radius > d;
 }
 
 
@@ -164,8 +175,16 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  // let fChar = '';
+  return str.split('').reduce((prev, cur, ind, arr) => {
+    if (prev === null) {
+      if (arr.indexOf(cur) === ind && arr.lastIndexOf(cur) === ind) {
+        return cur;
+      }
+    }
+    return prev;
+  }, null);
 }
 
 
@@ -191,8 +210,9 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const arr = [a, b].sort();
+  return `${isStartIncluded ? '[' : '('}${arr[0]}, ${arr[1]}${isEndIncluded ? ']' : ')'}`;
 }
 
 
@@ -208,8 +228,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -225,8 +245,10 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  let str = num.toString();
+  str = str.split('').reverse().join('');
+  return parseInt(str, 10);
 }
 
 
@@ -250,8 +272,29 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(inputNum) {
+  let digit;
+  const digits = (`${inputNum}`).split('').reverse();
+  let flag;
+  let sum;
+  let i1;
+  let len1;
+  flag = true;
+  sum = 0;
+
+  for (i1 = 0, len1 = digits.length; i1 < len1; i1 += 1) {
+    digit = digits[i1];
+    digit = parseInt(digit, 10);
+    flag = !flag;
+    if ((flag)) {
+      digit *= 2;
+    }
+    if (digit > 9) {
+      digit -= 9;
+    }
+    sum += digit;
+  }
+  return sum % 10 === 0;
 }
 
 /**
@@ -268,8 +311,13 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let rootSum = num;
+  do {
+    rootSum = rootSum.toString().split('').map(Number).reduce((prev, cur) => prev + cur, 0);
+  } while (rootSum > 9);
+
+  return rootSum;
 }
 
 
@@ -294,8 +342,23 @@ function getDigitalRoot(/* num */) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
+function isBracketsBalanced(expr) {
+  const stack = [];
+  const openB = ['[', '(', '{', '<'];
+  const closeB = [']', ')', '}', '>'];
+
+  for (let i = 0; i < expr.length; i += 1) {
+    if (openB.indexOf(expr[i]) > -1) {
+      stack.push(expr[i]);
+    } else if (closeB.indexOf(expr[i]) > -1) {
+      if (stack.length > 0 && openB.indexOf(stack[stack.length - 1]) === closeB.indexOf(expr[i])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
 
--- test.js
+++ test.js
@@ -1,50 +1,20 @@
-function chainPromises(array, action) {
-  return new Promise((resolve) => {
-    let count = array.length;
-    const retVals = new Array(array.length).fill(null);
-    array.map((item, ind) => {
-      item.then((value) => {
-        retVals[ind] = value;
-        count -= 1;
-        if (count === 0) {
-          resolve(retVals.filter((el) => el !== null).reduce((prev, cur) => {
-            if (prev === undefined) {
-              return cur;
-            }
-            if (cur === null) {
-              return prev;
-            }
-            return action(prev, cur);
-          }, undefined));
-        }
-      }).catch(() => {
-        count -= 1;
-        if (count === 0) {
-          resolve(retVals.filter((el) => el !== null).reduce((prev, cur) => {
-            if (prev === undefined) {
-              return cur;
-            }
-            if (cur === null) {
-              return prev;
-            }
-            return action(prev, cur);
-          }, undefined));
-        }
-      });
-      return item;
-    });
-  });
+function isBracketsBalanced(expr) {
+  const stack = [];
+  const openB = ['[', '(', '{', '<'];
+  const closeB = [']', ')', '}', '>'];
+
+  for (let i = 0; i < expr.length; i += 1) {
+    if (openB.indexOf(expr[i]) > -1) {
+      stack.push(expr[i]);
+    } else if (closeB.indexOf(expr[i]) > -1) {
+      if (stack.length > 0 && openB.indexOf(stack[stack.length - 1]) === closeB.indexOf(expr[i])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
-const lorem = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
-const promises = lorem.split(' ').map((item) => new Promise((resolve) => resolve(item)));
-
-const arrayForPromise = new Array(10).fill(0).map((_, idx) => idx);
-const result2 = chainPromises(arrayForPromise.map((item) => (item % 2
-  ? Promise.resolve(item)
-  : Promise.reject(Error(`Predictable Rejection ${item}`)))),
-(a, b) => a - b);
-
-// chainPromises(promises, (a, b) => `${a} ${b}`).then((val) => console.log(val));
-result2.then((val) => console.log(val));
-// console.log(chainPromises.toString());
+console.log(isBracketsBalanced('[]'));
